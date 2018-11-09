1d5681cf1ec87c494982c243b0e63aa978ce018c
Talha Saruhan <saruhan.talha@gmail.com>
2018-11-09 05:16:57 +0300

    Added Eigen benchmark

---
+++ Benchmarks/Eigen10K.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string.h>
+#include <thread>
+#include <chrono>
+#include <random>
+#include <sstream>
+#include <Eigen\Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main() {
+
+    const unsigned n = 10000;
+
+    MatrixXd matA = MatrixXd::Random(n, n);
+    MatrixXd matB = MatrixXd::Random(n, n);
+
+    setNbThreads(12);
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    MatrixXd matC = matA * matB;
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Runtime: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds.\n";
+
+
+    while (1) {}
+}
